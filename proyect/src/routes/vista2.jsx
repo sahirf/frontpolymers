c1ca340e0ce1bcc3171ae99847fc0f57d950c92c
sahirf <sahirvsegurav@gmail.com>
2024-04-13 14:57:26 -0500

    comprobando que la rama esta bien

---
+++ proyect/src/routes/vista2.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/nbar";
+import FondoAzul from "../../components/backg";
+export default function Vista2() {
+    return (
+    <html lang="en" >
+      <FondoAzul />
+      <Navbar />
+        
+    </html>
+  )
+}
