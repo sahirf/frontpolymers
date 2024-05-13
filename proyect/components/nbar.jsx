7ddbb9e9339b1e6791e770ff875988f3c5fe5ff7
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2024-05-13 11:50:39 -0500

    modificacion Link componente Navbar

--- proyect/components/nbar.jsx
+++ proyect/components/nbar.jsx
@@ -4,6 +4,7 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom';
 
 export default function Navbar_() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -12,10 +13,10 @@ export default function Navbar_() {
     <div >
       <nav className="mx-auto flex w-screen items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -28,15 +29,15 @@ export default function Navbar_() {
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-24 ">
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          <Link to="/mix_polymers" className="text-sm font-semibold leading-6 text-white">
             Mix Polymers
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          </Link>
+          <Link to="/mix_polymers" className="text-sm font-semibold leading-6 text-white">
             Seach For Polymers
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
+          </Link>
+          <Link to="/mix_polymers" className="text-sm font-semibold leading-6 text-white">
             Find Polymers
-          </a>
+          </Link>
         </Popover.Group>
       </nav>
       {/* AQUI EMPIEZA VISTA DE CELULAR  */}
@@ -46,14 +47,14 @@ export default function Navbar_() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-neutral-600"
@@ -66,24 +67,24 @@ export default function Navbar_() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="#"
+                <Link
+                  to="/mix_polymers"
                   className="-mx-3 block rounded-lg px-3 py-4 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Mix Polymers
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <Link
+                  to="/mix_polymers"
                   className="-mx-3 block rounded-lg px-3 py-4 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Seach For Polymers
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <Link
+                  to="/mix_polymers"
                   className="-mx-3 block rounded-lg px-3 py-4 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Find Polymers
-                </a>
+                </Link>
               </div>
               
             </div>
--- proyect/src/routes/vista2.jsx
+++ proyect/src/routes/vista2.jsx
@@ -1,24 +1,8 @@
 import FondoAzul from "../../components/backg";
 import Navbar_ from "../../components/nbar";
 import MultipleChoice from "../../components/multipleForm";
-// import axios from 'axios';
 
-export default function Vista2() {
- 
-
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   try {
-  //     const response = await axios.get('51.222.143.27:5000/')
-  //     const result = response.data
-  //     console.log(result)
-
-  //   } catch (error) {
-  //     console.error('Error sending data to API:', error);
-  //   }
-  // };
-    
+export default function Vista2() {  
     return (
       <div className="min-h-screen">
         <FondoAzul />
