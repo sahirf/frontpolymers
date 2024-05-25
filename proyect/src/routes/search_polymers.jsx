c8abf8bed95369f2274ad958094db152491f7953
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2024-05-25 15:08:00 -0500

    adelanto solucion

--- proyect/src/routes/search_polymers.jsx
+++ proyect/src/routes/search_polymers.jsx
@@ -1,89 +1,121 @@
-import { useState, useEffect } from 'react';
+import { Component } from 'react';
 import axios from 'axios';
 import FondoAzul from "../../components/backg";
 import Navbar_ from "../../components/nbar";
 
-export default function Vista5() {  
-  const [inputValue, setInputValue] = useState('');
-  const [data, setData] = useState(null); // Estado para almacenar los datos de la respuesta
-  const [error, setError] = useState(null); // Estado para almacenar errores
-  const [openIndex, setOpenIndex] = useState(null); // Estado para controlar qué acordeón está abierto
+class Vista5 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      data: null,
+      error: null,
+      openIndex: null,
+      subOpenIndex: null
+    };
+  }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`http://51.222.143.27:5000/all-types`, {
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+      const [responseTypes, responsePolymers2, responsePolymers4] = await Promise.all([
+        axios.get(`http://51.222.143.27:5000/types-from-type_id:2`, {
           headers: {
             'Content-Type': 'application/json'
           }
-        });
-        setData(response.data); // Almacena la respuesta en el estado
-      } catch (error) {
-        setError('Error fetching data');
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData(); // Llama a la función para obtener los datos
-  }, []); // El array vacío asegura que se ejecute una vez al montar el componente
-  
-  const toggleAccordion = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
+        }),
+        axios.get(`http://51.222.143.27:5000/polymers-from-type_id:2`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }),
+        axios.get(`http://51.222.143.27:5000/polymers-from-type_id:4`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      ]);
+
+      const data = {
+        name: "Comportamiento con la temperatura",
+        subItems: [
+          {
+            id: responseTypes.data[0][0],
+            name: responseTypes.data[0][1],
+            polymers: responsePolymers2.data
+          },
+          {
+            id: responseTypes.data[1][0],
+            name: responseTypes.data[1][1],
+            polymers: responsePolymers4.data
+          }
+        ]
+      };
+
+      this.setState({ data });
+    } catch (error) {
+      this.setState({ error: 'Error fetching data' });
+      console.error('Error fetching data:', error);
+    }
   };
 
+  toggleAccordion = (index) => {
+    this.setState({ openIndex: this.state.openIndex === index ? null : index });
+  };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
-    
+  toggleSubAccordion = (index) => {
+    this.setState({ subOpenIndex: this.state.subOpenIndex === index ? null : index });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      const response = await axios.get(`http://51.222.143.27:5000/all-types`, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      console.log('Response from API:', response.data);
+      // Aquí puedes realizar otra petición si es necesario
     } catch (error) {
       console.error('Error submitting form:', error);
     }
   };
-  
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value });
   };
 
-  
+  render() {
+    const { inputValue, data, error, openIndex, subOpenIndex } = this.state;
 
-  return (
-    <div className="min-h-screen">
-      <FondoAzul />
-      <Navbar_ />
-      <section>
-        <h1 className="text-white font-bold text-center text-2xl mt-2">Search polymers</h1>
-        <form onSubmit={handleSubmit} className="mx-auto max-w-lg">
-          <input 
-            type="text" 
-            value={inputValue} 
-            onChange={handleInputChange}
-            className="mt-4 w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-          />
-          <input 
-            type="submit" 
-            className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" 
-            value="Submit" 
-          />
-        </form>
-        {error && <p className="text-red-500">{error}</p>} {/* Muestra el error si existe */}
-        {data ? (
-          <div className="text-white mt-4">
-            {data.map((item, index) => (
-              <div key={item[0]} className="mb-4">
+    return (
+      <div className="min-h-screen">
+        <FondoAzul />
+        <Navbar_ />
+        <section>
+          <h1 className="text-white font-bold text-center text-2xl mt-2">Search polymers</h1>
+          <form onSubmit={this.handleSubmit} className="mx-auto max-w-lg">
+            <input 
+              type="text" 
+              value={inputValue} 
+              onChange={this.handleInputChange}
+              className="mt-4 w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+            />
+            <input 
+              type="submit" 
+              className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" 
+              value="Submit" 
+            />
+          </form>
+          {error && <p className="text-red-500">{error}</p>}
+          {data ? (
+            <div className="text-white mt-4">
+              <div key={data.name} className="mb-4">
                 <div
                   className="flex justify-between items-center bg-gray-800 text-white p-4 cursor-pointer"
-                  onClick={() => toggleAccordion(index)}
+                  onClick={() => this.toggleAccordion(data.name)}
                 >
-                  <h3 className="text-lg font-semibold">{item[1]}</h3>
+                  <h3 className="text-lg font-semibold">{data.name}</h3>
                   <svg
-                    className={`w-6 h-6 transform ${openIndex === index ? 'rotate-180' : ''}`}
+                    className={`w-6 h-6 transform ${openIndex === data.name ? 'rotate-180' : ''}`}
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -91,19 +123,54 @@ export default function Vista5() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
                   </svg>
                 </div>
-                {openIndex === index && (
+                {openIndex === data.name && (
                   <div className="bg-gray-700 text-white p-4">
-                    <p>{item[1]}</p> {/* Aquí puedes añadir más detalles si los tienes */}
+                    {data.subItems.map((subItem, index) => (
+                      <div key={subItem.id} className="mb-2">
+                        <div
+                          className="flex justify-between items-center bg-gray-800 text-white p-2 cursor-pointer"
+                          onClick={() => this.toggleSubAccordion(index)}
+                        >
+                          <h4 className="text-md font-semibold">{subItem.name}</h4>
+                          <svg
+                            className={`w-5 h-5 transform ${subOpenIndex === index ? 'rotate-180' : ''}`}
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                          </svg>
+                        </div>
+                        {subOpenIndex === index && (
+                          <div className="bg-gray-700 text-white p-4">
+                            {subItem.polymers ? (
+                              <div className="text-white">
+                                {subItem.polymers.map((polymer, polymerIndex) => (
+                                  <div key={polymerIndex} className="mb-2">
+                                    <div className="flex justify-between items-center bg-gray-800 text-white p-2">
+                                      <h4 className="text-md font-semibold">{polymer[1]}</h4>
+                                    </div>
+                                  </div>
+                                ))}
+                              </div>
+                            ) : (
+                              <p>Loading...</p>
+                            )}
+                          </div>
+                        )}
+                      </div>
+                    ))}
                   </div>
                 )}
               </div>
-            ))}
-          </div>
-        ) : (
-          <p className="text-white mt-4">Loading...</p> // Muestra un mensaje de carga mientras se obtienen los datos
-        )}
-      </section>
-
-    </div>
-  );
+            </div>
+          ) : (
+            <p className="text-white mt-4">Loading...</p>
+          )}
+        </section>
+      </div>
+    );
+  }
 }
+
+export default Vista5;
